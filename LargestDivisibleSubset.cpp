8b969798c193dab8801623c486dd4013ceebd02f
Avnish-Dewan <47629050+Avnish-Dewan@users.noreply.github.com>
2020-06-13 17:42:11 +0530

    Largest Divisible Subset
    Given a set of distinct positive integers, find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies:
    
    Si % Sj = 0 or Sj % Si = 0.
    
    If there are multiple solutions, return any subset is fine.

---
+++ LargestDivisibleSubset.cpp
@@ -0,0 +1,52 @@
+// Input: [1,2,3]
+// Output: [1,2] (of course, [1,3] will also be ok)
+
+// Input: [1,2,4,8]
+// Output: [1,2,4,8]
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int> nums) {
+        
+        int n = nums.size();
+        
+        if(n<=1){
+            return nums;
+        }
+        
+        
+        sort(nums.begin(),nums.end());
+        
+        
+        
+        vector<int> div(n,1);
+        vector<int> prev(n,-1);
+        
+        int ind = 0;
+        
+        for(int i=1;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(nums[i]%nums[j] == 0){
+                    if(div[i]<div[j]+1){
+                        div[i] = div[j]+1;
+                        prev[i] = j;
+                    }
+                }
+            }
+            
+            if (div[ind] < div[i]) 
+                ind = i; 
+        }
+        
+        int index = ind;
+        
+        vector<int> v;
+        
+        while(index>=0){
+            v.push_back(nums[index]);
+            index = prev[index];
+        }
+        
+        return v;
+    }
+};
